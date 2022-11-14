f82c7c4b3daa3489dcc783fa840914f3489962f4
Anton <antnbrnv@yandex.ru>
2022-11-14 17:54:59 +0300

    feat: creat FilmService

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -7,9 +7,13 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class Film {
+
+    private Set<Integer> usersLikes = new HashSet<>();
 
     private int id;
     @NotBlank
--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -1,7 +1,36 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.ObjectNotFoundException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.storage.FilmStorage;
+
+import java.util.Collection;
 
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class FilmService {
+    private final FilmStorage filmStorage;
+
+    public Film create(Film film) {
+        return filmStorage.create(film);
+    }
+
+    public Film update(Film film) {
+        return filmStorage.update(film);
+    }
+
+    public Collection<Film> findAll() {
+        log.info("Список фильмов отправлен");
+        return filmStorage.findAll();
+    }
+
+    public Film getByid(int id) {
+        if (!filmStorage.getFilms().containsKey(id)) {
+            throw new ObjectNotFoundException("Фильм не найден");
+        }
+    }
 }
