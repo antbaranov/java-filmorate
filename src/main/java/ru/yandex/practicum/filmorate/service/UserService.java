b8703c4616a56f832e8ec70b72373a45167d83a2
Anton <antnbrnv@yandex.ru>
2023-06-06 13:21:57 +0300

    fix

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -7,15 +7,7 @@ import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Feed;
 import ru.yandex.practicum.filmorate.model.User;
-
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
-
-
-
-import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
-
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
-
 
 import java.util.Collection;
 
